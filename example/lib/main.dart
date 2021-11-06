7594337f27ac1443ea410997fb94125ee9e411ed
gamako <gamako@gmail.com>
2021-11-06 16:48:27 +0900

    fix: sample project import error

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shared_preferences_flutter_riverpod/shared_preferences_riverpod.dart';
+import 'package:shared_preferences_riverpod/shared_preferences_riverpod.dart';
 
 late SharedPreferences prefs;
 
